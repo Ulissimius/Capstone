b6a06b1071aaa3ba05ebd395479095e0c1fd4cc4
Tyler Coudriet <tyler.coudriet@gmail.com>
2020-03-26 19:31:42 -0500

    added route for list page

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -18,6 +18,29 @@ module.exports = app => {
         }
 
         // Return a 200 OK status and send the html page
-        return res.render('pages/home/home.hbs')
+        return res.render('pages/home/home.hbs', {
+            assetUrl: '/pages/home/home'
+        })
+    })
+
+    app.get('/list', (req, res) => {
+        console.log('GET Request Received for List Page')
+
+        const authToken = req.cookies.authentication
+
+        if (authToken) {
+            Users.findByToken('login', authToken).then(user => {
+                if (!user) {
+                    console.log('No User Found')
+                } else {
+                    console.log(`Visitor is Logged In as ${user.username}`)
+                }
+            })
+        }
+
+        // Return a 200 OK status and send the html page
+        return res.render('pages/list/list.hbs', {
+            assetUrl: '/pages/list/list'
+        })
     })
 }
