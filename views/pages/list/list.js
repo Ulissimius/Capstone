a81ce07eee7b106404553a715d2dc66f69c58456
Ulissimius <mathewpavao@gmail.com>
2020-04-28 11:16:29 -0400

    Added wrapper element. Added Button function to "New Recipe Button". Added close button.

--- views/pages/list/list.js
+++ views/pages/list/list.js
@@ -2,41 +2,33 @@
 // let creRecView = document.querySelector(); //Creating a new recipe
 // let oldRecView = document.querySelector(); //Viewing a saved recipe
 // let selRecView = document.querySelector('#nr-select'); //Choosing how to create a recipe
+let testView = '';
+const wrapper = document.querySelector('#rp-wrapper');
+wrapper.parentElement.id
 function openView(view) {
     //if view is already open, do not open again.
+    if (testView != '') {
+        closeView(testView);
+    }
+    testView = view;
+
+    if (wrapper.style.display == "none") {
+        wrapper.style.display = "";
+    }
     let curView = document.querySelector(view); // Dynamically choose a view
-    curView.style.display = ""
+    curView.style.display = "";
 }
 
-function closeView(view) {
+function closeView(view, exit) {
     //if view is closed, do not close again.
+    let curView = document.querySelector(view); // Dynamically choose a view
+    curView.style.display = "none";
+    if (exit == true) {
+        wrapper.style.display = "none"     
+    } 
 }
 
 // Test Button JS
 var tButt = document.querySelector('#test-butt');
 var reveal = document.querySelectorAll('.click-me');
 var x = 0;
-
-tButt.addEventListener('click', e => {
-    if (x == 0) {
-        reveal[3].style.display = "none";
-        reveal[0].style.display = "block";
-        console.log(x);
-        x++;
-    } else if (x == 1) {
-        reveal[0].style.display = "none";
-        reveal[1].style.display = "block";
-        console.log(x);
-        x++;
-    } else if (x == 2) {
-        reveal[1].style.display = "none";
-        reveal[2].style.display = "block";
-        console.log(x);
-        x++;
-    } else if (x == 3) {
-        reveal[2].style.display = "none";
-        reveal[3].style.display = "block";
-        console.log(x);
-        x = 0;
-    }
-});
