8fd012cb0098dc12ff850dad1062feb60adf2711
Ulissimius <mathewpavao@gmail.com>
2020-04-28 11:54:05 -0400

    Added comments to list.js

--- views/pages/list/list.js
+++ views/pages/list/list.js
@@ -1,34 +1,43 @@
-// Add JS here
-// let creRecView = document.querySelector(); //Creating a new recipe
-// let oldRecView = document.querySelector(); //Viewing a saved recipe
-// let selRecView = document.querySelector('#nr-select'); //Choosing how to create a recipe
-let testView = '';
-const wrapper = document.querySelector('#rp-wrapper');
-wrapper.parentElement.id
-function openView(view) {
-    //if view is already open, do not open again.
-    if (testView != '') {
-        closeView(testView);
-    }
-    testView = view;
+// ******************** Title Sections with this template comments ********************
+// Example:
+// ******************** Recipe Card JS ********************
+// This JS handles filling out the data for the recipe cards.
 
-    if (wrapper.style.display == "none") {
+// ******************** Button JS ********************
+// Gives functionality to the various buttons on the page.
+
+// Declarations
+const wrapper = document.querySelector('#rp-wrapper'); // The wrapper is special div that holds floating windows.
+let prevView = null; // prevView holds the previous view id so it can be closed when you open a new view.
+
+function openView(view) { 
+    /* openView opens the passed view by setting display back to default.
+    openView also closes the previous view by saving the last view passed to it. */
+
+    let curView = document.querySelector(view); // Finds the current view element to open
+
+    if (wrapper.style.display == "none") { // Opens the wrapper div if it is closed.
         wrapper.style.display = "";
     }
-    let curView = document.querySelector(view); // Dynamically choose a view
-    curView.style.display = "";
+
+    if (prevView) { // Looks for a previous view and closes it if one is found.
+        closeView(prevView);
+    }
+
+    prevView = view; // sets the new previous view for the next call of openView
+
+    curView.style.display = ""; // Opens the current view
 }
 
 function closeView(view, exit) {
-    //if view is closed, do not close again.
-    let curView = document.querySelector(view); // Dynamically choose a view
-    curView.style.display = "none";
-    if (exit == true) {
+    /* closeView closes the passed view by setting display to none.
+    closeView will also close the wrapper if exit is passed in as true */
+
+    let curView = document.querySelector(view); // Finds the current view element to close
+
+    curView.style.display = "none"; // Closes the current view
+
+    if (exit == true) { // Closes the wrapper div if the x button was used.
         wrapper.style.display = "none"     
     } 
 }
-
-// Test Button JS
-var tButt = document.querySelector('#test-butt');
-var reveal = document.querySelectorAll('.click-me');
-var x = 0;
